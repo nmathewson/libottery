928b454390fbae69b6b7c17faffd5bddea82590b
Nick Mathewson <nickm@torproject.org>
2013-05-14 15:13:50 -0400

    Reorder ottery.h to put the easiest, most commonly used functions at the top.

--- src/ottery.h
+++ src/ottery.h
@@ -3,17 +3,20 @@
 #include <stdint.h>
 #include <sys/types.h>
 
-#ifndef OTTERY_INTERNAL
-struct __attribute__((aligned(16))) ottery_state {
-  uint8_t dummy_[1024];
-};
-struct ottery_config {
-  uint8_t dummy_[1024];
-};
-#else
-struct ottery_state;
 struct ottery_config;
-#endif
+struct ottery_state;
+
+/* Functions that use an implicit global state */
+void ottery_rand_bytes(void *out, size_t n);
+unsigned ottery_rand_unsigned(void);
+uint64_t ottery_rand_uint64(void);
+unsigned ottery_rand_range(unsigned top);
+uint64_t ottery_rand_range64(uint64_t top);
+
+int ottery_init(const struct ottery_config *cfg);
+void ottery_add_seed(const uint8_t *seed, size_t n);
+void ottery_wipe(void);
+void ottery_stir(void);
 
 /* Error codes */
 #define OTTERY_ERR_NONE                  0x0000
@@ -46,9 +49,17 @@ void ottery_set_fatal_handler(void (*fn)(int errorcode));
 #define OTTERY_CHACHA20_NO_SIMD "CHACHA20-NOSIMD"
 
 int ottery_config_init(struct ottery_config *cfg);
-int ottery_config_set_option(struct ottery_config *cfg, uint32_t option);
 int ottery_config_force_implementation(struct ottery_config *cfg,
                                        const char *impl);
+
+#ifndef OTTERY_INTERNAL
+struct __attribute__((aligned(16))) ottery_state {
+  uint8_t dummy_[1024];
+};
+struct ottery_config {
+  uint8_t dummy_[1024];
+};
+#endif
 
 /* Functions that take or manipulate state explicitly. */
 int ottery_st_init(struct ottery_state *st, const struct ottery_config *cfg);
@@ -62,19 +73,6 @@ unsigned ottery_st_rand_unsigned(struct ottery_state *st);
 uint64_t ottery_st_rand_uint64(struct ottery_state *st);
 unsigned ottery_st_rand_range(struct ottery_state *st, unsigned top);
 uint64_t ottery_st_rand_range64(struct ottery_state *st, uint64_t top);
-
-
-/* Functions that use an implicit global state */
-int ottery_init(const struct ottery_config *cfg);
-void ottery_add_seed(const uint8_t *seed, size_t n);
-void ottery_wipe(void);
-void ottery_stir(void);
-
-void ottery_rand_bytes(void *out, size_t n);
-unsigned ottery_rand_unsigned(void);
-uint64_t ottery_rand_uint64(void);
-unsigned ottery_rand_range(unsigned top);
-uint64_t ottery_rand_range64(uint64_t top);
 
 
 #endif
